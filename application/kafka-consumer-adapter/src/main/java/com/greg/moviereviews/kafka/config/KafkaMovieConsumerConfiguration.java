a36f3c33ee6137bc78475e9f4bcf6ebe7100e61d
Gregoire <gregoire.peterschmitt@gmail.com>
2025-01-07 20:21:58 +0100

    base de la consommation kafka avec spring-kafka

---
+++ application/kafka-consumer-adapter/src/main/java/com/greg/moviereviews/kafka/config/KafkaMovieConsumerConfiguration.java
@@ -0,0 +1,49 @@
+package com.greg.moviereviews.kafka.config;
+
+import java.util.HashMap;
+import java.util.Map;
+import lombok.AllArgsConstructor;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+
+@Configuration
+@AllArgsConstructor
+@EnableConfigurationProperties(KafkaMovieConsumerProperties.class)
+public class KafkaMovieConsumerConfiguration {
+
+  // TODO : MCI OK. Demarrage OK. Tenter la connexion avec le broker Kafka
+
+  private final KafkaMovieConsumerProperties kafkaMovieConsumerProperties;
+
+  @Bean
+  public ConsumerFactory<String, String> consumerFactory() {
+    Map<String, Object> props = new HashMap<>();
+    props.put(
+        ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
+        kafkaMovieConsumerProperties.getBootstrapServers());
+    props.put(ConsumerConfig.GROUP_ID_CONFIG, kafkaMovieConsumerProperties.getGroupId());
+    props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+    props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+    return new DefaultKafkaConsumerFactory<>(props);
+  }
+
+  @Bean
+  public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
+    ConcurrentKafkaListenerContainerFactory<String, String> factory =
+        new ConcurrentKafkaListenerContainerFactory<>();
+    factory.setConsumerFactory(consumerFactory());
+    return factory;
+  }
+
+  @KafkaListener(topics = "local_movie", groupId = "azd")
+  public void listenGroupFoo(String message) {
+    System.out.println("\n\n -- Received Message in group AZD: " + message);
+  }
+}
--- bootstrap/src/main/java/com/greg/moviereviews/bootstrap/MovieReviewsApplication.java
+++ bootstrap/src/main/java/com/greg/moviereviews/bootstrap/MovieReviewsApplication.java
@@ -1,16 +1,22 @@
 package com.greg.moviereviews.bootstrap;
 
 import com.greg.moviereviews.bootstrap.config.MovieReviewsConfiguration;
+import com.greg.moviereviews.kafka.config.KafkaMovieConsumerConfiguration;
 import com.greg.moviereviews.postgresql.adapter.config.PostgresqlConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication(scanBasePackages = "com.greg.moviereviews")
-@Import({MovieReviewsConfiguration.class, PostgresqlConfiguration.class})
+@Import({
+  MovieReviewsConfiguration.class,
+  PostgresqlConfiguration.class,
+  KafkaMovieConsumerConfiguration.class
+})
 public class MovieReviewsApplication {
 
   public static void main(String[] args) {
+
     SpringApplication.run(MovieReviewsApplication.class, args);
   }
 }
